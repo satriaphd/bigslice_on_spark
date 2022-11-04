e88105c2931b707b6df5a64353a6a6125ae25f44
Satria Kautsar <satria@pop-os.localdomain>
2022-11-04 14:55:18 -0400

    added support for dbfs file checking

--- axolotl/utils.py
+++ axolotl/utils.py
@@ -5,7 +5,7 @@ import re
 
 def check_file_exists(file_path):
     matches = re.match(
-        "^((?P<type>[a-z]+)://){0,1}(?P<path>[^#\\<\\>\\$\\+%!`&\\*'\\|\\{\\?\"=\\}:\\@]+)$",
+        "^((?P<type>[a-z]+):/{1,2}){0,1}(?P<path>[^#\\<\\>\\$\\+%!`&\\*'\\|\\{\\?\"=\\}:\\@]+)$",
         file_path
     )
     if matches == None:
@@ -18,13 +18,24 @@ def check_file_exists(file_path):
     
     if matches["type"] == "file":
         return path.exists(matches["path"])
+    elif matches["type"] == "dbfs":
+        try:
+            from pyspark.dbutils import DBUtils
+            dbutils = DBUtils(spark)
+            try:
+                dbutils.fs.ls(matches["path"])
+                return True
+            except:
+                return False
+        except ImportError:
+            raise Exception("can't access DataBricks DBUtils")
     else:
         raise NotImplementedError()
 
 
 def is_directory(file_path):
     matches = re.match(
-        "^((?P<type>[a-z]+)://){0,1}(?P<path>[^#\\<\\>\\$\\+%!`&\\*'\\|\\{\\?\"=\\}:\\@]+)$",
+        "^((?P<type>[a-z]+):/{1,2}){0,1}(?P<path>[^#\\<\\>\\$\\+%!`&\\*'\\|\\{\\?\"=\\}:\\@]+)$",
         file_path
     )
     if matches == None:
@@ -37,5 +48,21 @@ def is_directory(file_path):
     
     if matches["type"] == "file":
         return path.isdir(matches["path"])
+    elif matches["type"] == "dbfs":
+        try:
+            from pyspark.dbutils import DBUtils
+            dbutils = DBUtils(spark)
+            base_name = path.basename(matches["path"].rstrip("/"))
+            for file in dbutils.fs.ls(matches["path"]):
+                if file.name.rstrip("/") == base_name:
+                    if file.name.endswith("/"):
+                        return True
+                    else:
+                        return False
+                else:
+                    return True
+            return True
+        except ImportError:
+            raise Exception("can't access DataBricks DBUtils")
     else:
         raise NotImplementedError()
